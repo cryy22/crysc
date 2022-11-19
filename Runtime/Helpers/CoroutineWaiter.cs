0034b3a77a307c5853a9fe19299a21d84497bb88
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-19 01:00:03 -0800

    add CoroutineWaiter helper

---
+++ Runtime/Helpers/CoroutineWaiter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Crysc.Helpers
+{
+    public static class CoroutineWaiter
+    {
+        public static IEnumerator RunConcurrently<T>(
+            IEnumerable<T> behaviours,
+            Func<T, IEnumerator> enumerator
+        ) where T : MonoBehaviour
+        {
+            yield return RunConcurrently(
+                behaviours
+                    .Select(b => b.StartCoroutine(enumerator(b)))
+                    .ToArray()
+                );
+        }
+        
+        public static IEnumerator RunConcurrently(params Coroutine[] coroutines)
+        {
+            foreach (Coroutine coroutine in coroutines) { yield return coroutine; }
+        }
+    }
+}
