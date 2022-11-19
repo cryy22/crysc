eee155f0e83ab491ab2b3823e48dbf6cf26ae5e8
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-19 04:25:02 -0800

    add SingletonBehaviour generic

---
+++ Runtime/Singletons/SingletonBehaviour.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Singletons
+{
+    [DefaultExecutionOrder(-1)]
+    public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+    {
+        public static T I { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (I != null)
+            {
+                Debug.LogWarning("Multiple instances of singleton " + typeof(T).Name);
+                Destroy(gameObject);
+                return;
+            }
+
+            I = (T) this;
+        }
+    }
+}
