3d042783ca8b723aed866b154509c8a0774759c5
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-12-14 04:23:17 -0800

    add weighted random function

--- Runtime/Helpers/Randomizer.cs
+++ Runtime/Helpers/Randomizer.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
+using Random = UnityEngine.Random;
 
 namespace Crysc.Helpers
 {
@@ -11,6 +13,39 @@ namespace Crysc.Helpers
             {
                 int randomIndex = Random.Range(minInclusive: i, maxExclusive: list.Count);
                 (list[i], list[randomIndex]) = (list[randomIndex], list[i]);
+            }
+        }
+
+        public static T GetRandomElement<T>(IEnumerable<T> enumerable)
+        {
+            List<T> list = enumerable.ToList();
+            return list[Random.Range(minInclusive: 0, maxExclusive: list.Count)];
+        }
+
+        public static T GetWeightedRandomElement<T>(IEnumerable<T> enumerable, Func<T, int> weightFunc)
+        {
+            List<ElementWeight<T>> elementWeights = new();
+            var totalWeight = 0;
+
+            foreach (T element in enumerable)
+            {
+                elementWeights.Add(item: new ElementWeight<T>(element: element, minWeight: totalWeight));
+                totalWeight += weightFunc(element);
+            }
+
+            int randomWeight = Random.Range(minInclusive: 0, maxExclusive: totalWeight);
+            return elementWeights.Last(ew => randomWeight >= ew.MinWeight).Element;
+        }
+
+        private readonly struct ElementWeight<T>
+        {
+            public T Element { get; }
+            public int MinWeight { get; }
+
+            public ElementWeight(T element, int minWeight)
+            {
+                Element = element;
+                MinWeight = minWeight;
             }
         }
     }
