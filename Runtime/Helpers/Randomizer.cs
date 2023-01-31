5daab826644133415d6e967616688ac36b90a1e4
cryy22 <cry@anon-f77397bb01ed4c01.art>
2023-01-31 01:46:41 -0800

    get two promo choices per round

--- Runtime/Helpers/Randomizer.cs
+++ Runtime/Helpers/Randomizer.cs
@@ -16,10 +16,31 @@ namespace Crysc.Helpers
             }
         }
 
+        public static void RandomizeElements<T>(T[] array)
+        {
+            RandomizeElementsInternal(array: array, randomizationRounds: array.Length);
+        }
+
+        private static void RandomizeElementsInternal<T>(IList<T> array, int randomizationRounds)
+        {
+            for (var i = 0; i < randomizationRounds; i++)
+            {
+                int randomIndex = Random.Range(minInclusive: i, maxExclusive: array.Count);
+                (array[i], array[randomIndex]) = (array[randomIndex], array[i]);
+            }
+        }
+
         public static T GetRandomElement<T>(IEnumerable<T> enumerable)
         {
-            List<T> list = enumerable.ToList();
-            return list[Random.Range(minInclusive: 0, maxExclusive: list.Count)];
+            T[] array = enumerable.ToArray();
+            return array[Random.Range(minInclusive: 0, maxExclusive: array.Length)];
+        }
+
+        public static IEnumerable<T> GetRandomElements<T>(IEnumerable<T> enumerable, int count)
+        {
+            T[] array = enumerable.ToArray();
+            RandomizeElementsInternal(array: array, randomizationRounds: count);
+            return array.Take(count: count);
         }
 
         public static T GetWeightedRandomElement<T>(IEnumerable<T> enumerable, Func<T, int> weightFunc)
