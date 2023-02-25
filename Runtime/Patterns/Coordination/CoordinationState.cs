6bd7787597293a1865db8b0212ea950333851e5d
cryy22 <cry@anon-f77397bb01ed4c01.art>
2023-02-25 00:39:51 -0800

    extract BattleState from GameState

--- Runtime/Patterns/Coordination/CoordinationState.cs
+++ Runtime/Patterns/Coordination/CoordinationState.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 namespace Crysc.Patterns.Coordination
 {
-    public class CoordinationState : ScriptableObject
+    public abstract class CoordinationState : ScriptableObject
     { }
 }
