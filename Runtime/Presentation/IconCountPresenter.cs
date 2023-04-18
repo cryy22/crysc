224edeae1b18b71ffa010e3a445589d82c70262a
cryy22 <cry@anon-f77397bb01ed4c01.art>
2023-04-18 14:15:51 -0700

    icon count presenter shows numerals above a threshold

--- Runtime/Presentation/IconCountPresenter.cs
+++ Runtime/Presentation/IconCountPresenter.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace Crysc.Presentation
@@ -8,10 +9,27 @@ namespace Crysc.Presentation
     {
         [SerializeField] private T IconPrefab;
         [SerializeField] private Transform Container;
+        [SerializeField] private TMP_Text NumeralCounter;
+
+        [SerializeField] private int NumeralCounterThreshold = 4;
 
         private readonly List<T> _icons = new();
 
         public void Set(int count)
+        {
+            if (count >= NumeralCounterThreshold)
+            {
+                DisplayIconCount(1);
+                NumeralCounter.text = $"x {count.ToString()}";
+                NumeralCounter.gameObject.SetActive(true);
+                return;
+            }
+
+            DisplayIconCount(count);
+            NumeralCounter.gameObject.SetActive(false);
+        }
+
+        private void DisplayIconCount(int count)
         {
             int difference = count - _icons.Count;
 
