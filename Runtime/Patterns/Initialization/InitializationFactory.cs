75af4440ab4b2efa4626ed8a2e0ed3a2284d5da9
cryy22 <cry@anon-f77397bb01ed4c01.art>
2023-04-21 16:55:17 -0700

    units are configured with potential jobs

--- Runtime/Patterns/Initialization/InitializationFactory.cs
+++ Runtime/Patterns/Initialization/InitializationFactory.cs
@@ -6,21 +6,18 @@ namespace Crysc.Patterns.Initialization
         where T : InitializationBehaviour<TInitParams>
     {
         [SerializeField] private T Prefab;
-
-        public T Create(TConfig config)
-        {
-            T instance = Instantiate(Prefab);
-            instance.Initialize(GetInitParams(config));
-
-            return instance;
-        }
-
-        protected abstract TInitParams GetInitParams(TConfig config);
+        public abstract T Create(TConfig config);
+        protected T Instantiate() { return Instantiate(Prefab); }
     }
 
     public abstract class InitializationFactory<T, TConfig> : InitializationFactory<T, TConfig, TConfig>
         where T : InitializationBehaviour<TConfig>
     {
-        protected override TConfig GetInitParams(TConfig config) { return config; }
+        public override T Create(TConfig config)
+        {
+            T instance = Instantiate();
+            instance.Initialize(config);
+            return instance;
+        }
     }
 }
