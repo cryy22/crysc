0c14e2b1fa3db9282942dabdd1aa1786888c0b08
cryy22 <116713472+cryy22@users.noreply.github.com>
2023-07-15 13:52:57 -0700

    wire up new background images to background configurer

--- Runtime/Presentation/ParallaxBackground.cs
+++ Runtime/Presentation/ParallaxBackground.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Crysc.Helpers;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Crysc.Presentation
 {
@@ -11,30 +8,15 @@ namespace Crysc.Presentation
     {
         [SerializeField] private Transform FocalPoint;
         [SerializeField] private List<ParallaxLayer> Layers = new();
-        [SerializeField] private Image CurtainPanel;
 
         private readonly Dictionary<ParallaxLayer, Vector3> _layersInitialPositions = new();
         private Camera _camera;
-        private CryRoutine _curtainRoutine;
-        private Color _curtainClosedColor;
-        private Color _curtainOpenColor;
 
         private void Awake()
         {
             _camera = Camera.main;
             foreach (ParallaxLayer layer in Layers)
                 _layersInitialPositions.Add(key: layer, value: layer.Transform.position);
-        }
-
-        private void Start()
-        {
-            if (CurtainPanel)
-            {
-                Color initialColor = CurtainPanel.color;
-                _curtainClosedColor = new Color(r: initialColor.r, g: initialColor.g, b: initialColor.b, a: 0.5f);
-                _curtainOpenColor = new Color(r: initialColor.r, g: initialColor.g, b: initialColor.b, a: 0);
-                CurtainPanel.color = _curtainOpenColor;
-            }
         }
 
         private void Update()
@@ -46,28 +28,6 @@ namespace Crysc.Presentation
             );
 
             foreach (ParallaxLayer layer in Layers) UpdateLayer(layer: layer, vocalDeltaRatio: vocalDeltaRatio);
-        }
-
-        public void SetCurtain(bool isClosing)
-        {
-            _curtainRoutine?.Stop();
-            _curtainRoutine = new CryRoutine(enumerator: RunCurtain(isClosing: isClosing), behaviour: this);
-        }
-
-        private IEnumerator RunCurtain(bool isClosing)
-        {
-            Color initialColor = CurtainPanel.color;
-            Color targetColor = isClosing ? _curtainClosedColor : _curtainOpenColor;
-            var t = 0f;
-
-            while (t < 1)
-            {
-                t += Time.deltaTime / 1f;
-                CurtainPanel.color = Color.Lerp(a: initialColor, b: targetColor, t: t);
-                yield return null;
-            }
-
-            CurtainPanel.color = targetColor;
         }
 
         private void UpdateLayer(ParallaxLayer layer, Vector2 vocalDeltaRatio)
