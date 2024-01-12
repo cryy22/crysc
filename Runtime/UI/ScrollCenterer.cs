7b6d40aa189055dbf1084a308e8923c29a09cf50
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-01-11 19:12:56 -0800

    scroll to keep selected job in view on varmint ledger

---
+++ Runtime/UI/ScrollCenterer.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Crysc.UI
+{
+    public class ScrollCenterer : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect ScrollRect;
+        [SerializeField] private RectTransform ListItemParent;
+        [SerializeField] private float OffsetPercentage = 0.125f;
+
+        private GameObject _selected;
+
+        private void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == _selected) return;
+            Debug.Log("new thing selected!!");
+
+            _selected = EventSystem.current.currentSelectedGameObject;
+            if (ListItemParent.Cast<Transform>().Any(item => item.gameObject == _selected) == false) return;
+            Debug.Log("new thing is in the list of things we care about!!");
+
+            var target = _selected.GetComponent<RectTransform>();
+            if (!target) return;
+            Debug.Log("new thing has a recttransform!!");
+
+            float viewportWidth = ScrollRect.viewport.rect.width;
+            float contentWidth = ScrollRect.content.rect.width;
+
+            var targetCorners = new Vector3[4];
+            target.GetWorldCorners(targetCorners);
+
+            Vector2 minPosition = Vector2.positiveInfinity;
+            Vector2 maxPosition = Vector2.negativeInfinity;
+            foreach (Vector3 corner in targetCorners)
+            {
+                Vector3 localCorner = ScrollRect.content.InverseTransformPoint(corner);
+                minPosition = Vector2.Min(lhs: minPosition, rhs: localCorner);
+                maxPosition = Vector2.Max(lhs: maxPosition, rhs: localCorner);
+            }
+
+            minPosition -= target.rect.size * OffsetPercentage;
+            maxPosition += target.rect.size * OffsetPercentage;
+
+            float minHorizontalScroll = Mathf.Clamp01((maxPosition.x - viewportWidth) / (contentWidth - viewportWidth));
+            float maxHorizontalScroll = Mathf.Clamp01(minPosition.x / (contentWidth - viewportWidth));
+            ScrollRect.horizontalNormalizedPosition = Mathf.Clamp(
+                value: ScrollRect.horizontalNormalizedPosition,
+                min: minHorizontalScroll,
+                max: maxHorizontalScroll
+            );
+        }
+    }
+}
