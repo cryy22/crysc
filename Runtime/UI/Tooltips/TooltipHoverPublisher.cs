d8a7413b625206ca8e9b9c529c4d803b3ae9624b
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-01-27 01:06:16 -0800

    disable tooltip on card drag

--- Runtime/UI/Tooltips/TooltipHoverPublisher.cs
+++ Runtime/UI/Tooltips/TooltipHoverPublisher.cs
@@ -9,8 +9,12 @@ namespace Crysc.UI.Tooltips
         private static TooltipHoverPublisher _instance;
         public static TooltipHoverPublisher I => _instance ??= new TooltipHoverPublisher();
 
+        public bool Enabled { get; set; } = true;
+
         public void RegisterHover(ITooltipTargetProvider targetProvider)
         {
+            if (!Enabled) return;
+
             Hovered?.Invoke(
                 sender: this,
                 e: new TooltipHoverEventArgs(
