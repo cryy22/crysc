369c88e069e3c1827cda128be284a67b5af66738
cryy22 <116713472+cryy22@users.noreply.github.com>
2024-05-26 14:06:05 -0700

    wip: switching parallax bg for register/unregister pattern

--- Runtime/Presentation/ParallaxBackground.cs
+++ Runtime/Presentation/ParallaxBackground.cs
@@ -7,10 +7,26 @@ namespace Crysc.Presentation
 {
     public class ParallaxBackground : MonoBehaviour
     {
+        public enum Layer
+        {
+            SkyMesa,
+            Formation,
+            CactusForest,
+            PathBG,
+            TrailContent,
+            PathFG,
+            FGFoliage,
+            FGCactus0,
+            FGCactus1,
+            FGCactus2,
+        }
+
         [SerializeField] private Transform FocalPoint;
+        [SerializeField] private LayerSpeed[] LayerSpeeds;
         [SerializeField] private List<ParallaxLayer> Layers = new();
 
         private readonly Dictionary<Transform, Vector3> _transformsInitialPositions = new();
+        private readonly Dictionary<Layer, List<Transform>> _layersTransforms = new();
         private Camera _camera;
 
         private void Awake()
@@ -46,6 +62,13 @@ namespace Crysc.Presentation
         {
             public string Name;
             public Transform[] Transforms;
+            public float Speed;
+        }
+
+        [Serializable]
+        private struct LayerSpeed
+        {
+            public Layer Layer;
             public float Speed;
         }
     }
