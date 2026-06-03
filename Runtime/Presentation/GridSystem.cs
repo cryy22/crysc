ef6598a558e3d00b99055e22963eb58f9c549598
cryy22 <116713472+cryy22@users.noreply.github.com>
2026-06-03 13:23:53 -0700

    add grid system autogenerator

---
+++ Runtime/Presentation/GridSystem.cs
@@ -0,0 +1,201 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+namespace Crysc.Presentation
+{
+    [ExecuteAlways]
+    public class GridSystem : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [field: SerializeField] public Vector2 ReferenceResolution { get; private set; } = new(x: 1280, y: 720);
+        [field: SerializeField] public Color GridColor { get; private set; } = Color.orangeRed;
+        [field: SerializeField] public float GridLineWidth = 2f;
+        [field: SerializeField, ValueDropdown("GetSortingLayers")]
+        public string SortingLayerName { get; private set; }
+
+        [field: SerializeField] public MarginsType Margins { get; private set; }
+
+        [field: Header("Guides")]
+        [field: SerializeField] public float[] FieldWidths { get; private set; }
+        [field: SerializeField] public float[] FieldHeights { get; private set; }
+        [field: SerializeField] public float GutterWidth { get; private set; }
+        [field: SerializeField] public float GutterHeight { get; private set; }
+
+        private Camera _camera;
+        private Canvas _canvas;
+        private CanvasScaler _scaler;
+
+        private RectTransform _marginBox;
+
+        private void OnValidate()
+        {
+            EditorApplication.delayCall += () =>
+            {
+                if (this)
+                    Refresh();
+            };
+        }
+
+        private void Refresh()
+        {
+            // create missing objects
+            _camera = Camera.main;
+            if (!_canvas)
+            {
+                _canvas = new GameObject("GridSystemCanvas").AddComponent<Canvas>();
+                _scaler = _canvas.gameObject.AddComponent<CanvasScaler>();
+                _canvas.transform.SetParent(transform);
+            }
+
+            _canvas.worldCamera = _camera;
+            _canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            _canvas.sortingLayerName = SortingLayerName;
+            _scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            _scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            _scaler.matchWidthOrHeight = 0;
+            _scaler.referenceResolution = ReferenceResolution;
+
+            if (!_marginBox)
+            {
+                _marginBox = new GameObject("MarginBox").AddComponent<RectTransform>();
+                _marginBox.SetParent(_canvas.transform);
+                _marginBox.localScale = Vector3.one;
+                _marginBox.localPosition = Vector3.zero;
+                _marginBox.anchorMin = Vector3.zero;
+                _marginBox.anchorMax = Vector3.one;
+            }
+
+            _marginBox.offsetMin = new Vector2(x: Margins.Left, y: Margins.Bottom);
+            _marginBox.offsetMax = new Vector2(x: -Margins.Right, y: -Margins.Top);
+
+            for (int i = _marginBox.transform.childCount - 1; i >= 0; i--)
+                DestroyImmediate(_marginBox.transform.GetChild(i).gameObject);
+
+            var prototypeLine = new GameObject("PrototypeLine").AddComponent<Image>();
+            prototypeLine.rectTransform.SetParent(_canvas.transform);
+            prototypeLine.rectTransform.localScale = Vector3.one;
+            prototypeLine.rectTransform.localPosition = Vector3.zero;
+            prototypeLine.color = GridColor;
+
+            Image horizontalPrototypeLine = Instantiate(original: prototypeLine, parent: _marginBox);
+            horizontalPrototypeLine.gameObject.name = "HorizontalLine";
+            horizontalPrototypeLine.rectTransform.pivot = new Vector2(x: 0.5f, y: 1);
+            horizontalPrototypeLine.rectTransform.anchorMin = new Vector2(x: 0, y: 1);
+            horizontalPrototypeLine.rectTransform.anchorMax = new Vector2(x: 1, y: 1);
+            horizontalPrototypeLine.rectTransform.sizeDelta = new Vector2(x: 0, y: GridLineWidth);
+
+            Image verticalPrototypeLine = Instantiate(original: prototypeLine, parent: _marginBox);
+            verticalPrototypeLine.gameObject.name = "VerticalLine";
+            verticalPrototypeLine.rectTransform.pivot = new Vector2(x: 0, y: 0.5f);
+            verticalPrototypeLine.rectTransform.anchorMin = new Vector2(x: 0, y: 0);
+            verticalPrototypeLine.rectTransform.anchorMax = new Vector2(x: 0, y: 1);
+            verticalPrototypeLine.rectTransform.sizeDelta = new Vector2(x: GridLineWidth, y: 0);
+
+            try
+            {
+                // draw lines
+                Image top = Instantiate(original: prototypeLine, parent: _marginBox);
+                top.gameObject.name = "Margin (top)";
+                top.rectTransform.pivot = new Vector2(x: 0.5f, y: 0);
+                top.rectTransform.anchorMin = new Vector2(x: 0, y: 1);
+                top.rectTransform.anchorMax = new Vector2(x: 1, y: 1);
+                top.rectTransform.sizeDelta = new Vector2(x: GridLineWidth * 2, y: GridLineWidth);
+
+                Image bottom = Instantiate(original: prototypeLine, parent: _marginBox);
+                bottom.gameObject.name = "Margin (Bottom)";
+                bottom.rectTransform.pivot = new Vector2(x: 0.5f, y: 1);
+                bottom.rectTransform.anchorMin = new Vector2(x: 0, y: 0);
+                bottom.rectTransform.anchorMax = new Vector2(x: 1, y: 0);
+                bottom.rectTransform.sizeDelta = new Vector2(x: GridLineWidth * 2, y: GridLineWidth);
+
+                Image left = Instantiate(original: prototypeLine, parent: _marginBox);
+                left.gameObject.name = "Margin (Left)";
+                left.rectTransform.pivot = new Vector2(x: 1, y: 0.5f);
+                left.rectTransform.anchorMin = new Vector2(x: 0, y: 0);
+                left.rectTransform.anchorMax = new Vector2(x: 0, y: 1);
+                left.rectTransform.sizeDelta = new Vector2(x: GridLineWidth, y: GridLineWidth * 2);
+
+                Image right = Instantiate(original: prototypeLine, parent: _marginBox);
+                right.gameObject.name = "Margin (Right)";
+                right.rectTransform.pivot = new Vector2(x: 0, y: 0.5f);
+                right.rectTransform.anchorMin = new Vector2(x: 1, y: 0);
+                right.rectTransform.anchorMax = new Vector2(x: 1, y: 1);
+                right.rectTransform.sizeDelta = new Vector2(x: GridLineWidth, y: GridLineWidth * 2);
+
+                var yScan = 0f;
+                var initialField = true;
+                foreach (float fieldHeight in FieldHeights)
+                {
+                    Image line;
+                    if (!initialField)
+                    {
+                        yScan -= GutterHeight;
+                        line = Instantiate(original: horizontalPrototypeLine, parent: _marginBox);
+                        line.rectTransform.pivot = new Vector2(x: 0.5f, y: 0);
+                        line.rectTransform.anchoredPosition = new Vector2(x: 0, y: yScan);
+                    }
+
+                    yScan -= fieldHeight;
+                    line = Instantiate(original: horizontalPrototypeLine, parent: _marginBox);
+                    line.rectTransform.anchoredPosition = new Vector2(x: 0, y: yScan);
+                    initialField = false;
+                }
+
+                var xScan = 0f;
+                initialField = true;
+                foreach (float fieldWidth in FieldWidths)
+                {
+                    Image line;
+                    if (!initialField)
+                    {
+                        xScan += GutterWidth;
+                        line = Instantiate(original: verticalPrototypeLine, parent: _marginBox);
+                        line.rectTransform.pivot = new Vector2(x: 1, y: 0.5f);
+                        line.rectTransform.anchoredPosition = new Vector2(x: xScan, y: 0);
+                    }
+
+                    xScan += fieldWidth;
+                    line = Instantiate(original: verticalPrototypeLine, parent: _marginBox);
+                    line.rectTransform.anchoredPosition = new Vector2(x: xScan, y: 0);
+                    initialField = false;
+                }
+            }
+            finally
+            {
+                DestroyImmediate(prototypeLine.gameObject);
+                DestroyImmediate(horizontalPrototypeLine.gameObject);
+                DestroyImmediate(verticalPrototypeLine.gameObject);
+            }
+        }
+
+        private IEnumerable<string> GetSortingLayers()
+        {
+            return SortingLayer.layers.Select(l => l.name);
+        }
+
+        [Serializable]
+        public struct MarginsType
+        {
+            [BoxGroup("Margins")]
+            [SerializeField, HorizontalGroup("Margins/Row"), LabelText("T"), LabelWidth(10)] public float Top;
+            [SerializeField, HorizontalGroup("Margins/Row"), LabelText("R"), LabelWidth(10)] public float Right;
+            [SerializeField, HorizontalGroup("Margins/Row"), LabelText("B"), LabelWidth(10)] public float Bottom;
+            [SerializeField, HorizontalGroup("Margins/Row"), LabelText("L"), LabelWidth(10)] public float Left;
+        }
+#else // IF NOT THE UNITY EDITOR
+        private void Awake()
+        {
+            Destroy(gameObject);
+        }
+#endif
+    }
+}
---
+++ Runtime/Presentation/PixelLayoutTranslator.cs
@@ -0,0 +1,137 @@
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Crysc.Presentation
+{
+    // NOTE: assumes width in units is always 16, if it starts failing check whether CameraUnitWidthLocker is set to 16
+    // and working properly
+
+    [ExecuteAlways]
+    public class PixelLayoutTranslator : MonoBehaviour
+    {
+        private const float _roundingStep = 1 / 8f;
+        [field: SerializeField] public Vector2 ReferenceLayoutSize { get; private set; } = new(x: 1280, y: 720);
+
+        [field: FormerlySerializedAs("<UseSizeReferenceSprite>k__BackingField")]
+        [field: SerializeField] public bool UseSpriteRenderer { get; private set; }
+
+        [field: FormerlySerializedAs("<SizeReferenceSpriteRenderer>k__BackingField")]
+        [field: SerializeField]
+        [field: ShowIf("UseSpriteRenderer")] public SpriteRenderer SpriteRenderer { get; private set; }
+
+        [field: SerializeField] [field: ShowIf("@UseSpriteRenderer && RendererUsesSimpleDrawMode()")]
+        public float ReferenceSpriteScaleFactor { get; private set; } = 1;
+
+        [field: SerializeField] [field: DisableIf("@UseSpriteRenderer && RendererUsesSimpleDrawMode()")]
+        public Vector2 ElementSize { get; private set; } = new(x: 16, y: 16);
+
+        [ShowInInspector] public Vector2 ReferencePosition
+        {
+            get => GetReferencePosition();
+            set => SetWithReferencePosition(value);
+        }
+
+        private Camera _camera;
+        private Transform _transform;
+        private Vector2 _windowUnitSize;
+        private float _pixelsPerUnit;
+        private Vector2 _cornerOffsetUnits;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+            _transform = transform;
+
+            UpdateTranslationValues();
+        }
+
+        public Vector2 GetReferencePosition()
+        {
+            Vector2 unitPos = _transform.position;
+            unitPos.y *= -1;
+            unitPos += _windowUnitSize / 2; // shift world origin to top-right
+            unitPos -= _cornerOffsetUnits; // set unitPos to position of top-right corner of element
+
+            return new Vector2(
+                x: Mathf.Round(unitPos.x * _pixelsPerUnit / _roundingStep) * _roundingStep,
+                y: Mathf.Round(unitPos.y * _pixelsPerUnit / _roundingStep) * _roundingStep
+            );
+        }
+
+        public void SetWithReferencePosition(Vector2 referencePos)
+        {
+            Vector2 unitPos = referencePos / _pixelsPerUnit;
+            unitPos += _cornerOffsetUnits;
+            unitPos -= _windowUnitSize / 2;
+            unitPos.y *= -1;
+            _transform.position = unitPos;
+        }
+
+        private void UpdateTranslationValues()
+        {
+            _windowUnitSize = new Vector2(
+                x: _camera.orthographicSize * 2 * _camera.aspect,
+                y: _camera.orthographicSize * 2
+            );
+
+            _pixelsPerUnit = ReferenceLayoutSize.x / _windowUnitSize.x;
+            _cornerOffsetUnits = ElementSize / _pixelsPerUnit / 2;
+        }
+
+#if UNITY_EDITOR
+        private bool _wasUsingSpriteRenderer;
+
+        private void OnValidate()
+        {
+            _camera = Camera.main;
+            if (_camera == null)
+                return;
+
+            _transform = transform;
+
+            UpdateTranslationValues();
+
+            if (!UseSpriteRenderer || !SpriteRenderer)
+            {
+                _wasUsingSpriteRenderer = false;
+                return;
+            }
+
+            if (SpriteRenderer.drawMode == SpriteDrawMode.Simple)
+            {
+                ElementSize =
+                    SpriteRenderer.sprite.rect.size / ReferenceSpriteScaleFactor
+                    * new Vector2(
+                        x: Mathf.Abs(_transform.lossyScale.x),
+                        y: Mathf.Abs(_transform.lossyScale.y)
+                    );
+
+                UpdateTranslationValues();
+            }
+            else if (!_wasUsingSpriteRenderer)
+            {
+                ElementSize = SpriteRenderer.size * _pixelsPerUnit;
+                UpdateTranslationValues();
+            }
+            else
+            {
+                EditorApplication.delayCall += () =>
+                {
+                    if (!this || !SpriteRenderer)
+                        return;
+                    SpriteRenderer.size = ElementSize / _pixelsPerUnit;
+                };
+            }
+
+            _wasUsingSpriteRenderer = true;
+        }
+
+        private bool RendererUsesSimpleDrawMode()
+        {
+            return SpriteRenderer && (SpriteRenderer.drawMode == SpriteDrawMode.Simple);
+        }
+#endif
+    }
+}
